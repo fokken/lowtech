e9b952d4742e161d1f9e6d5bfce65e69ba64e14e
fokk <fokk@dwerb>
2015-04-12 10:16:38 +0200

    input...

--- fuzz/fuzzy_arma3.py
+++ fuzz/fuzzy_arma3.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 # arma3 weridness
 from scapy.all import *
+import sys
 
-conf.iface='eth0'
+interface = str(sys.argv[1])
+target = str(sys.argv[2])
+
+conf.iface=interface
 
 for n in range(0,100):
-	strangeness = Ether()/IP(dst='46.252.6.180')/UDP(dport=2302)/fuzz(Raw())
+	strangeness = Ether()/IP(dst=target)/UDP(dport=2302)/fuzz(Raw())
 	sendp(strangeness)
 
